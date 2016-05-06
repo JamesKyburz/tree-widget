53fb46b1ab1b21fd6552466dbe23c75447128844
James Kyburz <james.kyburz@gmail.com>
2016-05-06 20:17:05 +0200

    bugfix selected

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function Tree (react) {
     toggle: function (root) {
       return function (e) {
         var el = e.target
-        var className = root.entries.length && /A|SPAN|DIV/.test(el.nodeName) ? 'selected' : 'open'
+        var className = !root.entries.length || /A|SPAN/.test(el.nodeName) ? 'selected' : 'open'
         while (el.nodeName !== 'LI') el = el.parentNode
         var toggle = el.classList.contains(className)
         if (className === 'selected') {
